ee3dd0c763614c70d3eafefb0ae7e98d33b4a7ff
SMbengue99 <serignesalioumbengue9outlook.com>
2023-07-14 10:31:39 -0700

    last digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+int n;
+
+srand(time(0));
+
+n = rand() - RAND_MAX / 2;
+/* your code goes there */
+printf("%d ",n);
+
+if (n > 5)
+printf("and is greater than 5\n");
+if (n == 0)
+printf("and is 0\n");
+if (n < 6)
+printf("and is less than 6 and not 0\n");
+return (0);
+}
