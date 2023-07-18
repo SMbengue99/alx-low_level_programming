8578f3b10a8b9e4d7bc51e04a3b9beb224e6c8f4
SMbengue99 <serignesalioumbengue9outlook.com>
2023-07-18 06:24:51 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: 0 when successfull
+ */
+void print_alpha(void)
+{
+char c = 'a';
+while (c <='z')
+{
+putchar(c);
+c++;
+}
+putchar('\n');
+}
