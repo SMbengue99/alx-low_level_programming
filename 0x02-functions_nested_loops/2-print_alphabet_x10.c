3a23b60e7fb66ddbbcfbc2b7e1050d84ae15b4cf
SMbengue99 <serignesalioumbengue9outlook.com>
2023-07-18 07:05:51 -0700

    alpha

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x(10) - print all alphabet in lowercase
+ */
+
+void print_alphabet_x10(void)
+{
+int i=0;
+char c;
+while (i < 10)
+{
+c = 'a';
+while (c <= 'z')
+{
+putchar(c); 
+c++;
+}
+putchar('\n');
+i++;
+}
+}
