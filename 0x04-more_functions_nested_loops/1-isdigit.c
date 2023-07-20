b75885b6749d2cd8b1991b2b6d60c276da2e6802
SMbengue99 <137651597+SMbengue99@users.noreply.github.com>
2023-07-20 15:56:43 +0000

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -5,10 +5,11 @@
  * @c: is the char to be checked
  * Return: 1 if char is isdigit, otherwise 0.
 */
-
-int _isdigit(int c){
-if (c >= 48 && c <= 57){
-    return (1);
-}
-    return (0);
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+	return (1);
+	}
+	return (0);
 }
