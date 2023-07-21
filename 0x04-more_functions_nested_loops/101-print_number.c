e99f72728280b0612238e0f639f094d5d76ed071
SMbengue99 <serignesalioumbengue9outlook.com>
2023-07-20 18:01:55 -0700

    number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: the integer to print
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	int digit, divisor = 1;
+
+	if (n < 0)
+	{
+	_putchar('-');
+	n *= -1;
+	}
+	while (n / divisor > 9)
+	{
+	divisor *= 10;
+	}
+
+	while (divisor != 0)
+	{
+	digit = n / divisor;
+	_putchar(digit + '0');
+	n %= divisor;
+	divisor /= 10;
+	}
+	if (n == 0)
+	{
+	_putchar('0');
+	}
+}
