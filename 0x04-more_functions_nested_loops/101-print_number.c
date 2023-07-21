5ab00b377a68982d26fa6d2bb50072860c735817
SMbengue99 <serignesalioumbengue9outlook.com>
2023-07-20 18:20:41 -0700

    number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,27 +8,21 @@
  */
 void print_number(int n)
 {
-	int digit, divisor = 1;
+        int i = 1;
 
-	if (n < 0)
-	{
-	_putchar('-');
-	n *= -1;
-	}
-	while (n / divisor > 9)
-	{
-	divisor *= 10;
-	}
-
-	while (divisor != 0)
-	{
-	digit = n / divisor;
-	_putchar(digit + '0');
-	n %= divisor;
-	divisor /= 10;
-	}
-	if (n == 0)
-	{
-	_putchar('0');
-	}
+        if (n < 0)
+        {
+        _putchar('-');
+        n = -n;
+        }
+        while (n/i >= 10)
+        {
+        i *= 10;
+        }
+        while (i > 0)
+        {
+        _putchar(n / i + '0');
+        n %= i;
+        i /= 10;
+        }
 }
